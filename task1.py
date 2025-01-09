2373278d179e35aeabbd7e04a5c38421432cb1f1
nlwolsey <nathanlewisw079@gmail.com>
2025-01-09 10:30:55 -0800

    done

--- task1.py
+++ task1.py
@@ -1,6 +1,16 @@
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you")
 
 def C():
+    print("Hi")
+
+x = input("Enter a letter, A, B or C: ")
+if x == "A":
+    A()
+elif x == "B":
+    B()
+elif x == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,27 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+def game():
+    number = random.randint(1,10)
+    for i in range(4):
+        guess = int(input(f"Enter your #{i+1} guess: "))
+        if guess == number:
+            print(f"You got it! The number was {number}")
+            break
+        else:
+            print("Incorrect. Try again.")
+    print(f"You couldn't guess it, the number was {number}")
+    
+def title():
+    print("Welcome to the number guessing game!")
+    print("A random integer will be chosen between 1 and 10")
+    print("You will have 4 guesses to choose the right number.")
+    print("Good Luck!")
+     
+title()
+p_ready = input("Are you ready to play? (yes/no): ")
+if p_ready == 'yes' or 'Yes' or 'YES' or 'y':
+    game()
+
+    
